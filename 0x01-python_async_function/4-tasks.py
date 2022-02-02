8be08c261f48f123f13e8e437ac85d036bc79ce5
Louvani <paula.louvani@gmail.com>
2022-02-02 11:23:50 -0500

    4. Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" 4. Tasks"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """4. Tasks"""
+    delay_list = []
+    for index in range(n):
+        delay_list.append(await task_wait_random(max_delay))
+    done = await asyncio.gather(*delay_list)
+    return sorted(done)
