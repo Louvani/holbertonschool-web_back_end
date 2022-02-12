b2c0ce90c5c1b364063ba164e9ca6575e3a1d1ab
Louvani <paula.louvani@gmail.com>
2022-02-12 12:17:44 -0500

    pep8

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -7,10 +7,12 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     ''' Class BasicCache '''
+
     def put(self, key, item):
         ''' put a item to the dictionary '''
         if key and item:
             self.cache_data[key] = item
+
     def get(self, key):
         ''' Get the value associated with the given key '''
         return self.cache_data.get(key)
