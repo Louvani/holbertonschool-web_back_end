2ac3d360a03ce8360852bbee32f4deace6db722f
Louvani <paula.louvani@gmail.com>
2022-02-15 23:29:17 -0500

    9. Basic - User credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,32 +22,34 @@ class BasicAuth(Auth):
             return authorization_header[6:]
         return None
 
-def decode_base64_authorization_header(
-        self, base64_authorization_header: str) -> str:
-    """Returns the decoded value of a Base64 string
-    base64_authorization_header"""
-    if not base64_authorization_header or not isinstance(base64_authorization_header, str):
-        return None
-    try:
-        return base64.b64decode(
-            base64_authorization_header.encode('utf-8')).decode('utf-8')
-    except Exception:
-        return None
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Returns the decoded value of a Base64 string
+        base64_authorization_header"""
+        if not base64_authorization_header or not isinstance(
+                base64_authorization_header, str):
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header.encode('utf-8')).decode('utf-8')
+        except Exception:
+            return None
 
-def extract_user_credentials(
-        self, decoded: str) -> (str, str):
-    """returns the user email and password from the Base64 decoded value."""
-    if decoded is None:
-        return (None, None)
+    def extract_user_credentials(
+            self, decoded: str) -> (str, str):
+        """returns the user email and password from
+        the Base64 decoded value."""
+        if decoded is None:
+            return (None, None)
 
-    if not isinstance(decoded, str):
-        return (None, None)
+        if not isinstance(decoded, str):
+            return (None, None)
 
-    if ':' not in decoded:
-        return (None, None)
+        if ':' not in decoded:
+            return (None, None)
 
-    return (decoded[:decoded.find(':')], decoded[decoded.find(':') + 1:])
+        return (decoded[:decoded.find(':')], decoded[decoded.find(':') + 1:])
 
-def user_object_from_credentials(
-        self, user_email: str, user_pwd: str) -> TypeVar('User'):
-    """returns the User instance based on his email and password."""
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on his email and password."""
