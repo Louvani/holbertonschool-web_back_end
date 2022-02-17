7087fe6c36e50fd916e091fbfa9eda223fc937bd
Louvani <paula.louvani@gmail.com>
2022-02-17 16:56:45 -0500

    1. Log formatter

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -14,6 +14,29 @@ import os
 import re
 from typing import List
 
+import mysql.connector
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize RedactingFormatter
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values in incoming log records
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
