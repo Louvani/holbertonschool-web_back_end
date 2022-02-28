e758dfe39ada2669483b1a33519e8ee0be3c3bbb
Louvani <paula.louvani@gmail.com>
2022-02-28 10:48:10 -0500

    2. Find user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -13,6 +13,8 @@ class DB:
     """
     DB class
     """
+    session_args = ['id', 'email', 'hashed_password',
+                  'session_id', 'reset_token']
 
     def __init__(self):
         """
@@ -40,4 +42,18 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Return the first row in the users table after filtering
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        for arg in kwargs:
+            if arg not in self.session_args:
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
         return user
