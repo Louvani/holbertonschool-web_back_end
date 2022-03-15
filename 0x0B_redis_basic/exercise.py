ed89de427c7fa50978cec929fc7c589886914ae6
Louvani <paula.louvani@gmail.com>
2022-03-15 16:18:43 -0500

    2. Incrementing values

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,14 +1,50 @@
 #!/usr/bin/env python3
 '''Writing strings to Redis'''
 
+from typing import Union, Optional, Callable
 import redis
 import uuid
+from functools import wraps
+
+
+
+def count_calls(method: Callable) -> Callable:
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrap(self, *args, **kwargs):
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrap
+
 
 class Cache():
-    def __init__(self) -> None:
-        self._redis = redis.Redis()
 
-    def store(self, data) -> str:
+    parametrize = {}
+    '''Keep cahe data into deisdb'''
+
+    def __init__(self) -> None:
+        '''Initialice a instace of redis'''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+
+    @count_calls
+    def store(self, data: Union[bytes, str, int, float]) -> str:
+        '''Generate a raandom key for the data'''
         key = uuid.uuid4().__str__()
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        '''convert the data back to the desired format.'''
+        value =  self._redis.get(key)
+        if value == None  or fn == None:
+            return value
+        return fn(value)
+
+    def get_str():
+        pass
+
+    def get_int():
+        pass
