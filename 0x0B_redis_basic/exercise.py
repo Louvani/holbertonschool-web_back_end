f58bda89dde209168c53061bd4934153486f901d
Louvani <paula.louvani@gmail.com>
2022-03-15 16:47:42 -0500

    3. Storing lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -17,6 +17,20 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrap
 
+def call_history(method: Callable) -> Callable:
+    '''store the history of inputs and outputs for a particular function.'''
+    inputs = method.__qualname__ + ":inputs"
+    outputs = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrap(self, *args, **kwds):
+        """wrap of decorator"""
+        self._redis.rpush(inputs, str(args))
+        returned_method = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(returned_method))
+        return returned_method
+    return wrap
+
 
 class Cache():
 
@@ -28,6 +42,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[bytes, str, int, float]) -> str:
         '''Generate a raandom key for the data'''
