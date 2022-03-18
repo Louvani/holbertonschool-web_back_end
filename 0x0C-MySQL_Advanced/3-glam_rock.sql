ce17cf7c90bef56d580e8330a0f34ac2720835a0
Louvani <paula.louvani@gmail.com>
2022-03-17 19:22:14 -0500

    ADD FILTER WHERE STYLE IS GLAM ROCK

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan FROM metal_bands ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%gLAM ROCK%' ORDER BY lifespan DESC;
