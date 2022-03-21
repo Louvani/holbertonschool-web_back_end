9cc7c6bee51cf2b173ee1c6de6d194ee4a8b6112
Louvani <paula.louvani@gmail.com>
2022-03-21 18:02:47 -0500

    Email validation

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- 5. Email validation to sent
+DELIMITER $$
+CREATE TRIGGER resets
+BEFORE UPDATE ON users FOR EACH ROW
+BEGIN
+IF OLD.email != NEW.email THEN
+    SET NEW.valid_email = 0;
+END IF;
+END
+$$
