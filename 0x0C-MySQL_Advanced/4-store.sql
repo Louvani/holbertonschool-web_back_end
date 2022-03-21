7b04e32b77aeb2248fb748cad4e68af597a197f0
Louvani <paula.louvani@gmail.com>
2022-03-21 16:15:56 -0500

    comming back maybe

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -4,6 +4,6 @@ CREATE TRIGGER first_trigger
     AFTER INSERT
     ON orders FOR EACH ROW
 BEGIN
-UPDATE items SET quantity = items.quantity-NEW.number
-    WHERE items.name = NEW.item_name;
+UPDATE items SET quantity = quantity-NEW.number
+    WHERE name = NEW.item_name;
 END
