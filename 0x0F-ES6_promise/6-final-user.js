1a767ccddedb3601486307cf8aedde841c6bb355
Louvani <paula.louvani@gmail.com>
2022-04-03 13:24:55 -0500

    6. Handle multiple promises

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import { signUpUser } from './4-user-promise.js';
+import { uploadPhoto } from './5-photo-reject.js';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const user = await signUpUser(firstName, lastName);
   let photo;
   try {
     photo = await uploadPhoto(fileName);
-  } catch (err) {
-    photo = err.toString();
+  } catch (error) {
+    photo = error.toString();
   }
   return [
     { value: user, status: 'fulfilled' },
