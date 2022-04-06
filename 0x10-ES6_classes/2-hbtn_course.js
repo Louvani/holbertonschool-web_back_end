6dafe18921502e3c313b03febc1139e6207259d1
Louvani <paula.louvani@gmail.com>
2022-04-06 17:01:29 -0500

    2. A Course, Getters, and Setters

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,42 @@
-export default HolbertonCourse {
-  constructor(name, length, students) {i
-    this._name = name;
-    this._length = length;
-    this._students = students
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  // Getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Setters
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw TypeError('Students must be a array of Strings');
+    }
+    this._students = newStudents;
   }
 }
