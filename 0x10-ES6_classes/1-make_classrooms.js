55b15263eb3121d89c7a1f827395f1b14b2da566
Louvani <paula.louvani@gmail.com>
2022-04-06 16:23:18 -0500

    1. Let's make some classrooms

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,6 @@
 import ClassRoom from "./0-classroom.js";
 
+// why don't appear on github?
 export default function initializeRooms() {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
