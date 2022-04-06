cdfa428df0e8eb7e0442f4938f09208150353824
Louvani <paula.louvani@gmail.com>
2022-04-06 16:28:24 -0500

    Fix lint quoutes

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
+// eslint-disable-next-line import/extensions
 import ClassRoom from './0-classroom.js';
 
-// why don't appear on github?
 export default function initializeRooms() {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
