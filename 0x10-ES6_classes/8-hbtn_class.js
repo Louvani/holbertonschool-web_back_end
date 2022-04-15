0ca9da77d234ab6eda646104dd8af3f735731eb8
Louvani <paula.louvani@gmail.com>
2022-04-15 17:42:14 -0500

    8. Primitive - Holberton Class mandatory

---
+++ 0x10-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,35 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set size(newSize) {
+    if (typeof newSize !== 'number') {
+      throw TypeError('Size must be a number');
+    }
+    this._size = newSize;
+  }
+
+  set location(newLocation) {
+    if (typeof newLocation !== 'string') {
+      throw TypeError('Location must be a string');
+    }
+    this._location = newLocation;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._size;
+    }
+    return this._location;
+  }
+}
