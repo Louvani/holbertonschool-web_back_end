b60e3e113190950869274bf495d0f83bfe3fc70f
Louvani <paula.louvani@gmail.com>
2022-04-16 16:53:05 -0500

    9. Map data structure

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,5 @@
+export default function groceriesList() {
+  const arr = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
+  const grocery = new Map(array);
+  return grocery;
+}
