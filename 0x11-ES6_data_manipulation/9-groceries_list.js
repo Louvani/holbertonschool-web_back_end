8c05027fe8c18a3778209b5234a7a437d88072e3
Louvani <paula.louvani@gmail.com>
2022-04-16 17:00:30 -0500

    lint

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -1,7 +1,5 @@
 export default function groceriesList() {
-  const list = [
-    ['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]
-  ];
+  const list = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
   const grocery = new Map(list);
   return grocery;
 }
