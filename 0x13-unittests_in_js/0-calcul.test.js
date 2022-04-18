375859b83996783def9f2225fc325a980da70fa0
Louvani <paula.louvani@gmail.com>
2022-04-18 17:12:17 -0500

    0. Basic test with Mocha and Node assertion library

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,16 +1,19 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-describe('calculate Number', () => {
+describe('calculateNumber', () => {
   it('check output when parameters are okay'), () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(3.7, 1), 5);
-    assert.strictEqual(calculateNumber(1.7, 3), 5);
     assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(-1, -2), -3);
   }
   it('return NaN if arguments are incorrects'), () => {
     assert.strictEqual(isNaN(calculateNumber()), true);
-    assert.strictEqual(isNaN(calculateNumber(1)), true);
+    assert.strictEqual(isNaN(calculateNumber(5)), true);
+    assert.strictEqual(isNaN(calculateNumber('5', '7')), true);
   }
 });
