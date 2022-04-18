4de6b116543d84a529702038bd988f3387748988
Louvani <paula.louvani@gmail.com>
2022-04-18 17:03:58 -0500

    0. Basic test with Mocha and Node assertion library

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-desccribe('calculate Number', () => {
+describe('calculate Number', () => {
   it('check output when parameters are okay'), () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     assert.strictEqual(calculateNumber(3.7, 1), 5);
